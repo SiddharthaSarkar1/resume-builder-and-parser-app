f01dbc4a51e2b4da51862a4f0245da997e15589c
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-04-09 03:16:24 +0530

    setting up redux store

--- app/components/TopNavBar.tsx
+++ app/components/TopNavBar.tsx
@@ -3,6 +3,7 @@
 import { usePathname } from "next/navigation";
 import { cx } from "../lib/cx";
 import Link from "next/link";
+import Image from "next/image";
 
 const TopNavBar = () => {
   const pathname = usePathname();
@@ -13,21 +14,42 @@ const TopNavBar = () => {
       <header
         aria-label="Site Header"
         className={cx(
-          "flex h-[var(--top-nav-bar-height)] items-center border-b-2 border-gray-100 px-3 lg:px-12",
+          "flex h-[var(--top-nav-bar-height)] items-center border-b-2 border-gray-200 px-3 lg:px-12",
           isHomePage && "bg-dot"
         )}
       >
         <div className="flex h-10 w-full items-center justify-between">
           <Link href="/">
-            <h2 className="text-xl text-primary">
-              Build And Parse Your Resume
-            </h2>
+            <div className="flex items-center justify-center">
+              <Image
+                src={"assets/heart.svg"}
+                alt="logo img"
+                height={16}
+                width={16}
+                className="h-8 w-full animate-pulse"
+                priority
+              />
+              <h1 className="ml-3 text-xl whitespace-nowrap font-bold text-primary">
+                Resume Builder and Parser
+              </h1>
+            </div>
           </Link>
           <nav
             aria-label="Site Nav Bar"
             className="flex items-center gap-2 text-sm font-medium"
           >
-            
+            {[
+              ["/resume-builder", "Builder"],
+              ["/resume-parser", "Parser"],
+            ].map(([href, text]) => (
+              <Link
+                key={text}
+                className="rounded-md px-1.5 py-2 text-gray-500 font-medium focus-visible:bg-gray-100 lg:px-4"
+                href={href}
+              >
+                {text}
+              </Link>
+            ))}
           </nav>
         </div>
       </header>
